0e9a5606f4b51fcdb69215982f8a5e4cfc5e6dd7
manuelWeb <manuel@webdesignord.fr>
2014-10-12 17:49:27 +0200

    modifié fichier avisClt.js > gest evt et ief

--- README.md
+++ README.md
@@ -5,22 +5,29 @@ C:/Users/Manu/Documents/sites/gitRepository/avis_clients
 modification zone avis clients site Temps L 
 
 /**
- *  date = 091014 
- *  js   = decendre la bare de scroll de la hauteur
- *  du contenu des avis client affiché dynamiquement.
- */
+*  date = 091014 
+*  js   = decendre la bare de scroll de la hauteur
+*  du contenu des avis client affiché dynamiquement.
+*/
 
- /**
-  * correction style > lien Voir tous les avis chevauche 
-    les étoiles soit z-index (crade) soit replacer les étoile dans le flux.
-  */
+/**
+* correction style > lien Voir tous les avis chevauche 
+  les étoiles soit z-index (crade) soit replacer les étoile dans le flux.
+*/
 
- /**
-  *  date = 111014
-  *  js   = avisClt.js
-  *  modification js résolue > descendre le scroll
-  *  nouvelles modif : 
-  *  protéger les variables
-  *  ajouter gestionnaire évenements pour ie 7 et 8 
-  *  corrigé bug affichage btn "Donnez votre avis sur ce produit" ie7
-  */
+/**
+*  date = 111014
+*  js   = avisClt.js
+*  modification js résolue > descendre le scroll
+*  nouvelles modif : 
+*  protéger les variables
+*  ajouter gestionnaire évenements pour ie 7 et 8 
+*  corrigé bug affichage btn "Donnez votre avis sur ce produit" ie7
+*/
+
+/**
+* date    = 121414
+* fichier = avisClt.js
+* modifs  = protégé les variables; amélioration gestionnaire evnmt; corrigé bug affichage ie7
+*/
+
--- js/avisClt.js
+++ js/avisClt.js
@@ -1,74 +1,83 @@
-// chercher class dans élément ou document entier
-document.getElementsByClassName = function(className, elmt) {
- var selection = new Array();
- var regex = new RegExp("\\b" + className + "\\b");
-   // le second argument, facultatif
-   if(!elmt)
-    elmt = document;
-  else if(typeof elmt == "string")
-    elmt = document.getElementById(elmt);
-   // on sélectionne les éléments ayant la bonne classe
-   var elmts = elmt.getElementsByTagName("*");
-   for(var i=0; i<elmts.length; i++)
-    if(regex.test(elmts[i].className))
-     selection.push(elmts[i]);
-
-   return selection;
- };
-
- var btnAvis = document.getElementsByClassName('js-btn-avis');
-
- function masquer (argument) {
-   var argL = argument.length;
-   for (var i = 0; i < argL; i++) {
-    argument[i].style.display = 'none'
+(function(){
+  //ief pour protéger les variables
+  // chercher class dans élément ou document entier
+  document.getElementsByClassName = function(className, elmt) {
+    var selection = new Array();
+    var regex = new RegExp('(^| )'+className+'( |$)');
+    // le second argument, facultatif
+    if(!elmt)
+      elmt = document;
+    else if(typeof elmt == "string")
+      elmt = document.getElementById(elmt);
+    // on sélectionne les éléments ayant la bonne classe
+    var elmts = elmt.getElementsByTagName("*");
+    for(var i=0; i<elmts.length; i++){
+      if(regex.test(elmts[i].className))
+        selection.push(elmts[i]);
+      };
+      return selection;
   };
-};
 
-// fct descendre ascenseur (sroll y) pour voir les avis lors de leur ouverture
-var divHeight;
-var obj = document.getElementById('ctl00_ContentPlaceHolder1_UC_AVIS_DIV_AVIS');
-
-if(obj.offsetHeight){
- divHeight=obj.offsetHeight;
-}else if(obj.style.pixelHeight){
- divHeight=obj.style.pixelHeight;
-}
-function fct_scroller(y) {
-  console.log(y)
-  window.scrollBy(0,y);
-}
-// AFFICHER
-function afficher (argument) {
-  var argL = argument.length;
-  for (var i = 0; i < argL; i++) {
-    argument[i].style.display = 'block';
+  function masquer (argument) {
+    var argL = argument.length;
+    for (var i = 0; i < argL; i++) {
+      argument[i].style.display = 'none';
+    };
   };
-  fct_scroller(divHeight+45);// 45 = taille pagination avis
-};
 
+  // fct descendre ascenseur (sroll y) pour voir les avis lors de leur ouverture
+  var elmntHeight,
+      obj = document.getElementsByClassName('avis')[0];
+  // console.log(document.getElementsByClassName('avis'))
+  if(obj.offsetHeight){
+   elmntHeight=obj.offsetHeight;
+  }else if(obj.style.pixelHeight){
+   elmntHeight=obj.style.pixelHeight;
+  };
+  function fct_scroller(y) {
+    // console.log(y)
+    window.scrollBy(0,y);
+  }
+  // AFFICHER + scroll
+  function afficher (argument) {
+    var argL = argument.length;
+    for (var i = 0; i < argL; i++) {
+      argument[i].style.display = 'block';
+    };
+    fct_scroller(elmntHeight+45);// 45 = taille pagination avis
+  };
 
-function toggleDisplay(elmt) {
- var count = 0,
- elmntToHide = document.getElementsByClassName('line');
- if(btnAvis[0].className == 'js-btn-avis avis-off'){
+  var btnAvis = document.getElementsByClassName('js-btn-avis');
+
+  function toggleDisplay(elmt) {
+    var count = 0,
+    elmntToHide = document.getElementsByClassName('line');
+    if(btnAvis[0].className == 'js-btn-avis avis-off'){
       // ajout class avis-on sur btn "Voir tous les avis"
       while(count<elmntToHide.length){
-       elmt[0].className = 'js-btn-avis avis-on';
-       count+=1;
-     };
-     afficher(elmntToHide);
-   }else{
-    elmt[0].className = 'js-btn-avis avis-off';
-    masquer(elmntToHide);
+        elmt[0].className = 'js-btn-avis avis-on';
+        count+=1;
+      };
+      afficher(elmntToHide);
+    }else{
+      elmt[0].className = 'js-btn-avis avis-off';
+      masquer(elmntToHide);
+    };
   };
-};
-// on masque les avis au chargement du doc > JS est activé
-toggleDisplay(btnAvis)
+  // on masque les avis au chargement du doc > JS est activé
+  toggleDisplay(btnAvis)
+  // addEvent ie + modern browser
+  function addEvent(el, e, f) {
+    if (el.attachEvent) {
+      return el.attachEvent('on'+e, f);
+    }
+    else {
+      return el.addEventListener(e, f, false);
+    };
+  };
+  // ajout du gestionnaire d'evnmt sur btn "Voir tous les avis"
+  addEvent(btnAvis[0],'click',function (e) {
+    toggleDisplay(btnAvis);
+  });
 
-// ajout du gestionnaire d'evnmt sur btn "Voir tous les avis"
-if(btnAvis[0].addEventListener){
-  btnAvis[0].addEventListener('click', function(e){toggleDisplay(btnAvis);} , false);
-}else {
-  btnAvis[0].attachEvent("onclick",function(e){toggleDisplay(btnAvis);});
-}
+})();
